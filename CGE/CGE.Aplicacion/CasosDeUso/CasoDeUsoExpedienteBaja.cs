0b439ad3d8983d744819387362102c93de7a2e80
Mateo Matus <110347934+SkillFP@users.noreply.github.com>
2024-06-20 13:02:07 -0300

    Null ServicioAutorizacion

--- CGE/CGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
+++ CGE/CGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
@@ -3,11 +3,13 @@ public class CasoDeUsoExpedienteBaja(IExpedienteRepositorio repoExp, ITramiteRep
 {
     public void EjecutarExpedienteBaja(int idExpediente, Usuario usuario)
     {
-        if(!servicio.PoseeElPermiso(usuario.permisos, Permiso.ExpedienteBaja)){
+        if (!servicio.PoseeElPermiso(usuario.permisos, Permiso.ExpedienteBaja))
+        {
             new AutorizacionException($"El usuario con id: {usuario.id} no posee permisos para dar de baja expedientes.");
         }
         Expediente? e = repoExp.consultaPorId(idExpediente);
-        if((e.Tramites != null)&&(e.Tramites.Count != 0)){
+        if ((e.Tramites != null) && (e.Tramites.Count != 0))
+        {
             foreach (Tramite t in e.Tramites)
             {
                 repoTra.darDeBajaTramite(t.Id);
--- CGE/CGE.Aplicacion/Servicios/ServicioAutorizacion.cs
+++ CGE/CGE.Aplicacion/Servicios/ServicioAutorizacion.cs
@@ -1,6 +1,15 @@
 namespace CGE.Aplicacion;
-public class ServicioAutorizacion: IServicioAutorizacion{
-    public bool PoseeElPermiso(List<Permiso>permisos, Permiso permiso){
-        return permisos.Contains(permiso);
+public class ServicioAutorizacion : IServicioAutorizacion
+{
+    public bool PoseeElPermiso(List<Permiso>? permisos, Permiso permiso)
+    {
+        if (permisos != null)
+        {
+            return permisos.Contains(permiso);
+        }
+        else
+        {
+            return false;
+        }
     }
 }
