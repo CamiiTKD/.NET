2d18cc72e2fabe8c9566d5d362267544e5119266
Mateo Matus <110347934+SkillFP@users.noreply.github.com>
2024-06-20 13:26:35 -0300

    CasoDeUsoTramiteConsultaTodos

---
+++ CGE/CGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteConsultaTodos.cs
@@ -0,0 +1,11 @@
+﻿namespace CGE.Aplicacion;
+
+public class CasoDeUsoTramiteConsultaTodos(ITramiteRepositorio repo)
+{
+    public List<Tramite>? EjecutarConsultarTodos()
+    {
+        List<Tramite>? lista;
+        lista = repo.ConsultarTodos();
+        return lista;
+    }
+}
