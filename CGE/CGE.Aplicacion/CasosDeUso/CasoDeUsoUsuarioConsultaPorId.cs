de4836a0e3ce04335bc4afeb7aab3df39318e65c
juanTarrioAKD <juantarriotkd@gmail.com>
2024-06-21 13:23:07 -0300

    agrego validadores

--- CGE/CGE.Aplicacion/CasosDeUso/CasoDeUsoUsuarioConsultaPorId.cs
+++ CGE/CGE.Aplicacion/CasosDeUso/CasoDeUsoUsuarioConsultaPorId.cs
@@ -1,5 +1,5 @@
 namespace CGE.Aplicacion;
-public class CasoDeUsoUsuarioConsultaPorId(IUsuarioRepositorio repo, IServicioAutorizacion servicio)
+public class CasoDeUsoUsuarioConsultaPorId(IUsuarioRepositorio repo)
 {
     public Usuario? EjecutarConsulta (int idUsuario){
         return repo.ConsultaUsuario(idUsuario);
--- CGE/CGE.Aplicacion/Enumerativos/Permiso.cs
+++ CGE/CGE.Aplicacion/Enumerativos/Permiso.cs
@@ -9,4 +9,5 @@ public enum Permiso{
     PermisoModificacion,
     UsuarioBaja,
     UsuarioModificacion
+
 }
